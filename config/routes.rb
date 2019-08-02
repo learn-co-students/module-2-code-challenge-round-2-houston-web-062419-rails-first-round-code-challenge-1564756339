273049310352da866508203e11d59c8758c3f324
abideen11 <abideenkareem17@gmail.com>
2019-08-02 11:04:28 -0500

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/sweets', to: "sweets#index", as: "sweets"
-  get '/vendors', to: "vendors#index", as: "vendors"
+  # get '/sweets', to: "sweets#index", as: "sweets"
+  # get '/vendors', to: "vendors#index", as: "vendors"
+  resources :vendors
+  resources :sweets
 
 end
